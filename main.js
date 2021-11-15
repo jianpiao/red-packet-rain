13c2e611b7c899f59d24a0dee5f11e259dd2d9dd
zhoujianpiao <zhoujianpiao@ghzhushou.com>
2021-11-15 20:29:48 +0800

    feat: 调整游戏流程

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ const gameOptions = {
   selfPool: 10,
   selfPic: 'redpacket',
   rate: 0.15,
-  maxSpeed: 1500,
-  minSpeed: 800,
+  maxSpeed: 200,
+  minSpeed: 50,
   max: 95,
-  time: 20, // 游戏时长
+  time: 10, // 游戏时长
   awardText: "恭喜获得女票一位"
 }
 
@@ -46,12 +46,14 @@ class TheGame {
       this.load.baseURL = GAME + "/";
     }
 
-    // 如果是桌面端则自适应
-    if (!game.device.desktop) {
-      this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-      this.scale.forcePortrait = true;
-      this.scale.refresh();
-    }
+    // 将游戏覆盖整个屏幕，同时保持其比例
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+    // 游戏水平居中
+    game.scale.pageAlignHorizontally = true;
+
+    // 游戏垂直居中
+    game.scale.pageAlignVertically = true;
 
     // 设置舞台背景色
     game.stage.backgroundColor = '#000';
@@ -77,6 +79,7 @@ class TheGame {
   create() {
     // 物理系统
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 2000;
 
     // 背景图
     let launchBg = game.add.sprite(0, 0, 'launchBg');
@@ -84,7 +87,7 @@ class TheGame {
     launchBg.height = game.height;
 
     // 开始按钮背景
-    this.gameStartButton = game.add.button(game.world.centerX, game.world.centerY + 200, "gameStartBtn", function () {
+    this.gameStartButton = game.add.button(game.world.centerX, gameOptions.gameHeight - 300, "gameStartBtn", function () {
       this.gameStartButton.visible = false
       this.gameStartText.visible = false
       this.cursorHandle.visible = false
@@ -94,11 +97,11 @@ class TheGame {
 
     // 游戏开始文案
     const gameStartTextStyle = { fill: '#FFF', fontSize: '30px', fontWeight: 'bolder' }
-    this.gameStartText = game.add.text(game.world.centerX, game.world.centerY + 249, '开始游戏', gameStartTextStyle)
-      .anchor.set(0.5, 0.5)
+    this.gameStartText = game.add.text(game.world.centerX, gameOptions.gameHeight - 250, '开始游戏', gameStartTextStyle)
+    this.gameStartText.anchor.set(0.5, 0.5)
 
     // 手指
-    this.cursorHandle = game.add.sprite(game.world.centerX - 36, game.world.centerY + 240, 'cursorAnimation')
+    this.cursorHandle = game.add.sprite(game.world.centerX - 36, gameOptions.gameHeight - 250, 'cursorAnimation')
     this.cursorHandle.animations.add('cursorAnimation')
     this.cursorHandle.play('cursorAnimation', 2, true);
   }
@@ -118,7 +121,6 @@ class TheGame {
     bgRainer.width = game.width;
     bgRainer.height = game.height;
 
-
     this.config = gameOptions;
     this.redPacketGroup = game.add.group();
     this.redPacketGroup.enableBody = true;
@@ -126,7 +128,6 @@ class TheGame {
     this.redPacketGroup.setAll('anchor.y', 1)
     this.redPacketGroup.setAll('outOfBoundsKill', true);
     this.redPacketGroup.setAll('checkWorldBounds', true);
-    this.maxWidth = game.width + 300;
     game.time.events.loop(Phaser.Timer.SECOND * gameOptions.rate, this.createRedPacket, this);
 
     // 初始化游戏时间
@@ -137,54 +138,117 @@ class TheGame {
     this.leftTimeText.fixedToCamera = true;
     this.leftTimeText.cameraOffset.setTo(game.camera.width - rfuc(80), rfuc(20));
 
-    // 创建一个关闭组
-    this.closeGroup = game.add.group()
-
-    // 遮罩
-    const hexGraphics = new Phaser.Graphics().beginFill(0x000000, 0.5).drawRect(0, 0, gameOptions.width, gameOptions.height);
-    this.pausedMask = game.add.sprite(0, 0, hexGraphics.generateTexture())
-    this.closeGroup.add(this.pausedMask)
-    // 退出游戏对话框
-    this.exitDialog = game.add.sprite(rfuc(62), rfuc(150), 'dialogExit')
-    this.closeGroup.add(this.exitDialog)
-    // 退出按钮
-    this.exitButton = game.add.button(rfuc(80), rfuc(315), 'buttonExit')
-    this.closeGroup.add(this.exitButton)
-    // 取消按钮
-    this.cancelButton = game.add.button(rfuc(200), rfuc(315), 'buttonCancel')
-    this.closeGroup.add(this.cancelButton)
-
-    // 设置组默认为不显示
-    this.closeGroup.visible = false
-
     // 重复切换帧，第一个参数是一帧间隔时长
     game.time.events.repeat(Phaser.Timer.SECOND, this.leftTime, this.refreshTime, this)
 
-    //添加按钮,并绑定事件
-    const closeImg = game.add.button(rfuc(20), rfuc(20), 'close', function () {
-      game.paused = true
-      this.gameAudio.pause()
-      this.closeGroup.visible = true
-      // 添加点击事件
-      game.input.onDown.add(this.handleExitGame, this)
-    }.bind(this))
+    // 添加右上角退出游戏按钮
+    game.add.button(rfuc(20), rfuc(20), 'close', this.showExitDialog, this)
+
+    // 创建退出游戏对话框
+    this.createExitDialog()
   }
 
-  // 结束游戏
+  // 创建红包
+  createRedPacket() {
+    const redPacket = this.redPacketGroup.getFirstExists(false); // 从组里抽取一个精灵元素
+    if (redPacket) {
+      redPacket.events.onInputDown.removeAll();
+      redPacket.loadTexture(this.config.selfPic)
+      redPacket.alpha = 1; // 设置透明度为1
+      redPacket.angle = 0 // 红包旋转角度
+      redPacket.reset(game.rnd.integerInRange(0, gameOptions.gameWidth - 144), 0)  //红包生成的位置
+      redPacket.body.velocity.x = game.rnd.integerInRange(-200, 100);   //红包移动的速度
+      redPacket.body.velocity.y = game.rnd.integerInRange(gameOptions.minSpeed, gameOptions.maxSpeed);
+      redPacket.inputEnabled = true;
+      redPacket.events.onInputDown.add(this.hitRedPacket, this) // 添加点击事件
+    }
+  }
+
+  // 点击红包
+  hitRedPacket(sprite) {
+    // 选中的红包设置为不可点击
+    sprite.inputEnabled = false;
+    // 给红包添加帧动画
+    let anim = sprite.animations.add(gameOptions.selfPic);
+    // 执行帧动画
+    sprite.play(gameOptions.selfPic, 20, false);
+    // 帧动画切换完毕后，执行补间动画，让红包渐变消失
+    anim.onComplete.add(this.killed, this, sprite)
+  }
+
+  createExitDialog() {
+    // 创建一个关闭组
+    this.closeGroup = game.add.group()
+    // 遮罩
+    const hexGraphics = new Phaser.Graphics().beginFill(0x000000, 0.5).drawRect(0, 0, gameOptions.width, gameOptions.height);
+    const pausedMask = game.add.sprite(0, 0, hexGraphics.generateTexture())
+    // 退出游戏对话框
+    const exitDialog = game.add.sprite(game.world.centerX, game.world.centerY, 'dialogExit')
+    exitDialog.anchor.set(0.5, 0.5)
+    // 退出按钮
+    this.exitButton = game.add.button(game.world.centerX - 130, game.world.centerY + 310, 'buttonExit')
+    this.exitButton.anchor.set(0.5, 0.5)
+
+    // 取消按钮
+    this.cancelButton = game.add.button(game.world.centerX + 130, game.world.centerY + 310, 'buttonCancel')
+    this.cancelButton.anchor.set(0.5, 0.5)
+
+    this.closeGroup.add(pausedMask)
+    this.closeGroup.add(exitDialog)
+    this.closeGroup.add(this.exitButton)
+    this.closeGroup.add(this.cancelButton)
+
+    this.closeGroup.visible = false
+  }
+
+  showExitDialog() {
+    game.paused = true
+    this.gameAudio.pause()
+    this.closeGroup.visible = true
+    game.input.onDown.add(this.handleExitGame, this)
+  }
+
+  // 退出游戏
   handleExitGame() {
-    let exitRect = new Phaser.Rectangle(rfuc(80), rfuc(315), 194, 66).copyFrom(this.exitButton);
-    let cancelRect = new Phaser.Rectangle(rfuc(200), rfuc(315), 194, 66).copyFrom(this.cancelButton);
-    if (cancelRect.contains(game.input.x, game.input.y)) { // 监听取消事件
-      game.input.onDown.remove(this.handleExitGame, this)
-      this.closeGroup.visible = false
-      game.paused = false
-      this.gameAudio.play() // 继续游戏
-    } else if (exitRect.contains(game.input.x, game.input.y)) { // 监听退出事件
+    let exitRect = new Phaser.Rectangle(game.world.centerX - 130, game.world.centerY + 310, 194, 66).copyFrom(this.exitButton);
+    let cancelRect = new Phaser.Rectangle(game.world.centerX + 130, game.world.centerY + 310, 194, 66).copyFrom(this.cancelButton);
+    if (this.calcContains(exitRect, game.input.x, game.input.y)) { // 监听取消事件
       game.input.onDown.remove(this.handleExitGame, this)
       this.closeGroup.visible = false
       game.paused = false
       this.restartGame() // 退回启动页（重新开始）
+    } else if (this.calcContains(cancelRect, game.input.x, game.input.y)) { // 监听退出事件
+      game.input.onDown.remove(this.handleExitGame, this)
+      this.closeGroup.visible = false
+      game.paused = false
+      this.gameAudio.play() // 继续游戏
     }
+  }
+
+  calcContains(sprite, inputX, inputY) {
+    const { width, height, x, y } = sprite
+    const maxWidth = x + width - width / 2
+    const minWidth = x - width / 2
+    const maxHeight = y + height
+    const minHeight = y - width / 2
+    if (inputX >= minWidth && inputX <= maxWidth && inputY >= minHeight && inputY <= maxHeight) {
+      return true
+    }
+    return false
+  }
+
+  // 退出按钮
+  handleExitBtn() {
+    this.closeGroup.visible = false
+    game.paused = false
+    this.restartGame() // 退回启动页（重新开始）
+  }
+
+  // 取消按钮
+  handleCancelBtn() {
+    this.closeGroup.visible = false
+    game.paused = false
+    this.gameAudio.play() // 继续游戏
   }
 
   // 刷新时间
@@ -199,34 +263,6 @@ class TheGame {
     }
   }
 
-  // 创建红包
-  createRedPacket() {
-    const redPacket = this.redPacketGroup.getFirstExists(false);
-    if (redPacket) {
-      redPacket.events.onInputDown.removeAll();
-      redPacket.loadTexture(this.config.selfPic)
-      redPacket.alpha = 1; // 设置透明度为1
-      redPacket.angle = 0 // 红包旋转角度
-      redPacket.reset(game.rnd.integerInRange(100, this.maxWidth), 100)  //红包生成的位置
-      redPacket.body.velocity.x = game.rnd.integerInRange(-500, -350);   //红包移动的速度
-      redPacket.body.velocity.y = game.rnd.integerInRange(gameOptions.minSpeed, gameOptions.maxSpeed);
-      redPacket.inputEnabled = true;
-      redPacket.events.onInputDown.add(this.hitRedPacket, this) // 添加点击事件
-    }
-  }
-
-  // 点击红包
-  hitRedPacket(sprite) {
-    // 选中的红包设置为不可点击
-    sprite.inputEnabled = false;
-    // 给红包添加帧动画
-    let anim = sprite.animations.add(gameOptions.selfPic);
-    // 执行帧动画
-    sprite.play(gameOptions.selfPic, 40, false);
-    // 帧动画切换完毕后，执行补间动画，让红包渐变消失
-    anim.onComplete.add(this.fade, this, sprite)
-  }
-
   // 展示奖励
   showAwardDialog() {
     this.showAwardGourp = game.add.group()
@@ -236,9 +272,11 @@ class TheGame {
     // 遮罩
     const pausedMask = game.add.sprite(0, 0, hexGraphics.generateTexture())
     // 对话框背景图
-    const openDialog = game.add.sprite(rfuc(62), rfuc(150), 'openRedpacket')
+    const openDialog = game.add.sprite(game.world.centerX, game.world.centerY, 'openRedpacket')
+    openDialog.anchor.set(0.5, 0.5)
     // 开奖硬币图
-    this.showAwardCoin = game.add.sprite(rfuc(130), rfuc(300), 'open')
+    this.showAwardCoin = game.add.button(game.world.centerX, game.world.centerY + 120, 'open', this.handleOpenCoin.bind(this))
+    this.showAwardCoin.anchor.set(0.5, 0.5)
     // 支持点击
     this.showAwardCoin.inputEnabled = true;
     // 将上面添加进组里，方便统一管理
@@ -247,63 +285,47 @@ class TheGame {
     this.showAwardGourp.add(this.showAwardCoin)
 
     // 结果背景图
-    const result = game.add.sprite(rfuc(0), rfuc(120), 'redpacketResult')
+    const result = game.add.sprite(game.world.centerX, game.world.centerY, 'redpacketResult')
+    result.anchor.set(0.5, 0.5)
     // 去用券按钮
-    this.showAwardUserTicket = game.add.sprite(rfuc(78), rfuc(445), 'buttonUseTicket')
+    this.showAwardUserTicket = game.add.button(game.world.centerX - 130, game.world.centerY + 310, 'buttonUseTicket', this.handleAwardDialogBtn.bind(this))
+    this.showAwardUserTicket.anchor.set(0.5, 0.5)
     // 继续抢按钮
-    this.showAwardGoOn = game.add.sprite(rfuc(198), rfuc(445), 'buttonContinue')
+    this.showAwardGoOn = game.add.button(game.world.centerX + 130, game.world.centerY + 310, 'buttonContinue', this.handleAwardDialogBtn.bind(this))
+    this.showAwardGoOn.anchor.set(0.5, 0.5)
     // 将上面添加进组里，方便统一管理
     this.showAwardResultGroup.add(result)
     this.showAwardResultGroup.add(this.showAwardUserTicket)
     this.showAwardResultGroup.add(this.showAwardGoOn)
     // 先设置抽奖结果为隐藏
     this.showAwardResultGroup.visible = false
-    // 添加按钮点击事件
-    game.input.onDown.add(this.handleOpenCoin, this)
   }
 
   // 点击拆红包按钮
   handleOpenCoin() {
-    //游戏暂停时,点击事件无效,只能通过这种画热点的形式来绑定事件
-    let openRect = new Phaser.Rectangle(rfuc(130), rfuc(315), 239, 239).copyFrom(this.showAwardCoin);
-    // 点击开奖
-    if (openRect.contains(game.input.x, game.input.y)) {
-      if (!this.canClickCoin) return
-      this.canClickCoin = false
-      this.openCoin = game.add.tween(this.showAwardCoin).to({ angle: -20 }, 100, 'Linear', true, 0, 6, true);
-      this.openCoin.onComplete.add(this.getAwardResult, this)
-    }
+    if (!this.canClickCoin) return
+    this.canClickCoin = false
+    this.openCoin = game.add.tween(this.showAwardCoin).to({ angle: -20 }, 100, 'Linear', true, 0, 6, true);
+    this.openCoin.onComplete.add(this.getAwardResult, this)
   }
 
-  // 开奖结果
+  // 显示开奖结果
   getAwardResult() {
-    this.canClickCoin = true
     document.getElementById('audioOpen').play()
     let ticketStyle = { fill: '#ffe67d', fontSize: '46px', fontWeight: 'bolder' }
-    let ticketText = game.add.text(0, rfuc(338), gameOptions.awardText, ticketStyle)
+    let ticketText = game.add.text(game.world.centerX, game.world.centerY + 75, gameOptions.awardText, ticketStyle)
+    ticketText.anchor.set(0.5, 0.5)
     ticketText.left = game.world.centerX - ticketText.width / 2 // 文字居中
     this.showAwardResultGroup.add(ticketText) // 添加进结果组里
     this.showAwardGourp.visible = false // 隐藏开奖前对话框
     this.showAwardResultGroup.visible = true // 显示开奖结果对话框
-    game.input.onDown.add(this.handleAwardDialogBtn, this) // 添加开奖结果按钮点击事件
   }
 
-  // 开奖按钮点击事件
+  // 去用券/继续抢
   handleAwardDialogBtn() {
-    let userTicketRect = new Phaser.Rectangle(rfuc(78), rfuc(445), 194, 66).copyFrom(this.showAwardUserTicket);
-    let continueRect = new Phaser.Rectangle(rfuc(198), rfuc(445), 194, 66).copyFrom(this.showAwardGoOn);
-
-    if (userTicketRect.contains(game.input.x, game.input.y)) { // 去用券
-      game.input.onDown.remove(this.handleAwardDialogBtn, this);
-      game.paused = false
-      this.restartGame()
-    } else if (continueRect.contains(game.input.x, game.input.y)) { // 继续抢
-      game.input.onDown.remove(this.handleAwardDialogBtn, this);
-      game.paused = false
-      this.restartGame()
-    }
+    game.paused = false
+    this.restartGame()
   }
-
 
   // 销毁
   killed(sprite) {
@@ -322,6 +344,7 @@ class TheGame {
   restartGame() {
     this.leftTime = gameOptions.time
     this.leftTimeText.text = this.leftTime;
+    this.canClickCoin = true
     game.state.restart()
   }
 }
